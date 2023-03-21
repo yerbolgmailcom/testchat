ed957ca481fdb78bbc35aea6f5fe75b1b39c1838
Yerbol Akhmetbekov <yerbol@gmail.com>
2023-03-20 20:18:57 -0500

    feat: add chat fucntionality

--- aichat/chat.py
+++ aichat/chat.py
@@ -9,3 +9,17 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 result = openai.Model.list()
 
 print(result)
+
+
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": "Hello!"}
+    ]
+)
+
+print(completion.choices[0].message)
+
+
+
+
